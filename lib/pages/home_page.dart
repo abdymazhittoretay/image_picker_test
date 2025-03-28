a63384f883166dcc091c3a0f927142d031b92d80
Toretay <abdymvzhit@gmail.com>
2025-03-28 16:49:25 +0500

    added methods to pick and make a photo

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -20,14 +21,50 @@ class _HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            ElevatedButton(onPressed: () {}, child: Text("Pick from gallery")),
+            ElevatedButton(
+              onPressed: () {
+                _selectFromGallery();
+              },
+              child: Text("Pick from gallery"),
+            ),
             SizedBox(height: 10.0),
-            ElevatedButton(onPressed: () {}, child: Text("Make a photo")),
-            SizedBox(height: 10.0),
-            selectedImage != null ? Image.file(selectedImage!) : SizedBox(),
+            ElevatedButton(
+              onPressed: () {
+                _makeAPhoto();
+              },
+              child: Text("Make a photo"),
+            ),
+            SizedBox(height: 16.0),
+            selectedImage != null
+                ? Image.file(selectedImage!)
+                : Text("Select image or make a photo!"),
           ],
         ),
       ),
     );
+  }
+
+  Future<void> _selectFromGallery() async {
+    final returnedXFile = await ImagePicker().pickImage(
+      source: ImageSource.gallery,
+    );
+
+    if (returnedXFile == null) return;
+
+    setState(() {
+      selectedImage = File(returnedXFile.path);
+    });
+  }
+
+  Future<void> _makeAPhoto() async {
+    final returnedXFile = await ImagePicker().pickImage(
+      source: ImageSource.camera,
+    );
+
+    if (returnedXFile == null) return;
+
+    setState(() {
+      selectedImage = File(returnedXFile.path);
+    });
   }
 }
