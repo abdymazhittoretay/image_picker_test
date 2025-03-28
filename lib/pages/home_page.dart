4b862453b690f24c3e93ccf7966516aad16ac135
Toretay <abdymvzhit@gmail.com>
2025-03-28 16:45:35 +0500

    created body with buttons and image

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,11 +10,23 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  File? selectedImage;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Image Picker"),
+      appBar: AppBar(title: Text("Image Picker")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed: () {}, child: Text("Pick from gallery")),
+            SizedBox(height: 10.0),
+            ElevatedButton(onPressed: () {}, child: Text("Make a photo")),
+            SizedBox(height: 10.0),
+            selectedImage != null ? Image.file(selectedImage!) : SizedBox(),
+          ],
+        ),
       ),
     );
   }
